7e6b2a7ffb6fccf9ea9abc239cc43db86d70e57b
dominique kon-sun-tack <dominique.k@elico-corp.com.sg>
2019-04-29 12:24:55 +0800

    [NEW] vision_pricelist price per uom

---
+++ vision_pricelist_uom/__manifest__.py
@@ -0,0 +1,31 @@
+##############################################################################
+#
+# Copyright (c) 2019 Alitec Pte Ltd All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly advised to contract support@modoolar.com
+#
+##############################################################################
+{
+
+    'name': "Vision Pricelist",
+    'summary': """Pricelist Rules with Uom""",
+    'category': 'Sale',
+    'version': '11.0.1',
+        'description': """
+Customised Pricelist for Vision Stationery
+===========================================
+    - last update: 27-APR-2019
+    
+        
+    """,
+
+    'author': "Alitec Pte Ltd",
+    'depends': ['sale'
+    ],
+    'data': ['views/pricelist.xml',
+    ],
+}
