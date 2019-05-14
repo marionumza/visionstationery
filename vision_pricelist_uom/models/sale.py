d01d34dd3d2067ab31151c1581e6c4a02fda4e2b
piyush <piyush@piyush-HP-Spectre-Pro-x360-G1>
2019-05-14 13:11:20 +0530

    set pricelist on sale order line and price will changed as per selected pricelist on line

--- vision_pricelist_uom/models/sale.py
+++ vision_pricelist_uom/models/sale.py
@@ -6,14 +6,43 @@ from odoo.exceptions import ValidationError
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+    
+    pricelist_id = fields.Many2one('product.pricelist', string='Pricelist', required=True, help="Pricelist for current sales order Line.")
 
+    
+#     @api.multi
+#     @api.onchange('pricelist_id')
+#     def orderline_pricelist_id_change(self):
+#         return self.product_id_change()
+        
     @api.multi
-    @api.onchange('product_id')
+    def _get_display_price(self, product):
+        # TO DO: move me in master/saas-16 on sale.order
+        pricelist_id = self.pricelist_id
+        if not pricelist_id:
+            pricelist_id = self.order_id.pricelist_id
+            
+        if pricelist_id.discount_policy == 'with_discount':
+            return product.with_context(pricelist=pricelist_id.id).price
+        product_context = dict(self.env.context, partner_id=self.order_id.partner_id.id, date=self.order_id.date_order, uom=self.product_uom.id)
+        final_price, rule_id = pricelist_id.with_context(product_context).get_product_price_rule(self.product_id, self.product_uom_qty or 1.0, self.order_id.partner_id)
+        base_price, currency_id = self.with_context(product_context)._get_real_price_currency(product, rule_id, self.product_uom_qty, self.product_uom, pricelist_id.id)
+        if currency_id != pricelist_id.currency_id.id:
+            base_price = self.env['res.currency'].browse(currency_id).with_context(product_context).compute(base_price, pricelist_id.currency_id)
+        # negative discounts (= surcharge) are included in the display price
+        return max(base_price, final_price)
+    
+    
+    @api.multi
+    @api.onchange('product_id','pricelist_id')
     def product_id_change(self):
         if not self.product_id:
             return {'domain': {'product_uom': []}}
-
         vals = {}
+        pricelist_id = self.pricelist_id
+        if not pricelist_id:
+            pricelist_id = self.order_id.pricelist_id
+        
         domain = {'product_uom': [('category_id', '=', self.product_id.uom_id.category_id.id)]}
         if not self.product_uom or (self.product_id.uom_id.id != self.product_uom.id):
             vals['product_uom'] = self.product_id.uom_id
@@ -24,7 +53,7 @@ class SaleOrderLine(models.Model):
             partner=self.order_id.partner_id.id,
             quantity=vals.get('product_uom_qty') or self.product_uom_qty,
             date=self.order_id.date_order,
-            pricelist=self.order_id.pricelist_id.id,
+            pricelist=pricelist_id.id,
             uom=self.product_uom.id,
             product=self.product_id.id
         )
@@ -51,17 +80,17 @@ class SaleOrderLine(models.Model):
 
         self._compute_tax_id()
 
-        if self.order_id.pricelist_id and self.order_id.partner_id:
+        if pricelist_id and self.order_id.partner_id:
             vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(
                 self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
         self.update(vals)
 
         pricelist_item_obj = self.env['product.pricelist.item']
-        #         if self.order_id.pricelist_id.pricelist_type == 'tender':
-        pricelist_item_ids = pricelist_item_obj.search([('pricelist_id', '=', self.order_id.pricelist_id.id),
+        #         if pricelist_id.pricelist_type == 'tender':
+        pricelist_item_ids = pricelist_item_obj.search([('pricelist_id', '=', pricelist_id.id),
                                                         ('product_id', '=', product.id)])
         if not pricelist_item_ids:
-            pricelist_item_ids = pricelist_item_obj.search([('pricelist_id', '=', self.order_id.pricelist_id.id),
+            pricelist_item_ids = pricelist_item_obj.search([('pricelist_id', '=', pricelist_id.id),
                                                             ('product_tmpl_id', '=', product.product_tmpl_id.id)])
 
         for pricelist_item in pricelist_item_ids:
@@ -73,18 +102,23 @@ class SaleOrderLine(models.Model):
 
         return result
 
-    @api.onchange('product_uom', 'product_uom_qty')
+    @api.onchange('product_uom', 'product_uom_qty','pricelist_id')
     def product_uom_change(self):
         if not self.product_uom or not self.product_id:
             self.price_unit = 0.0
             return
-        if self.order_id.pricelist_id and self.order_id.partner_id:
+        
+        pricelist_id = self.pricelist_id
+        if not pricelist_id:
+            pricelist_id = self.order_id.pricelist_id
+            
+        if pricelist_id and self.order_id.partner_id:
             product = self.product_id.with_context(
                 lang=self.order_id.partner_id.lang,
                 partner=self.order_id.partner_id.id,
                 quantity=self.product_uom_qty,
                 date=self.order_id.date_order,
-                pricelist=self.order_id.pricelist_id.id,
+                pricelist=pricelist_id.id,
                 uom=self.product_uom.id,
                 fiscal_position=self.env.context.get('fiscal_position'),
                 product=self.product_id.id
