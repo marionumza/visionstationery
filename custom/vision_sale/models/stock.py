d2f04ad60f7915d773df67ebfa5fe99d430a92e0
dominique kon-sun-tack <dominique.k@elico-corp.com.sg>
2020-01-08 09:22:40 +0800

    [ORG] reorganise the folder structure

---
+++ custom/vision_sale/models/stock.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError, UserError
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    urgency = fields.Selection([('normal', 'Normal'),
+                                ('urgent', 'Urgent'),
+                                ('very_urgent', 'Very Urgent')], string='Urgency', default='normal')
+
+    portal_requester = fields.Char('Portal Requester')
+    portal_requester_info = fields.Char('Portal Requester info')
+
+    @api.multi
+    def action_done(self):
+        self.ensure_one()
+        if self.picking_type_id.code == 'outgoing':
+            for move in self.move_lines:
+                if move.product_uom_qty < move.quantity_done:         # and (move.state not in ('done', 'cancel')):
+                    raise UserError(_('You cannot send more than the original demand'))
+        return super(StockPicking, self).action_done()
+
